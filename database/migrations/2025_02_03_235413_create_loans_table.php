045468ab2d27f2c27e5f86f60a2b0e3bbe4f3f92
Joao Paulo <joaobembe@icloud.com>
2025-02-03 21:24:16 -0300

    migration Loans

---
+++ database/migrations/2025_02_03_235413_create_loans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("borrowed_by_user_id")->constrained("users"); //primeiro usuario que emprestou a chave
+            $table->foreignId("given_by_user_id")->constrained("users");//usuario que deu a chave
+            $table->foreignId("borrowed_key_id")->constrained("keys"); //a dita cuja chave emprestada
+            $table->foreignId("returned_by_user_id")->constrained("users");//o usuario que devolveu a chave
+            $table->foreignId("receiver_user_id")->constrained("users");//o usuario que recebeu a chave na guarita
+            $table->timestamp("borrowed_at")->useCurrent();
+            $table->timestamp("returned_at");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
